b902c73d26905b288f2c28f160f05c8898ddf91f
bleeeana <bobkov.v03@icloud.com>
2025-03-07 04:05:59 +0300

    docker starts

--- kg_library/__init__.py
+++ kg_library/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://neo4j:7687")
-NEO4J_AUTH = os.getenv("NEO4J_AUTH", "neo4j/testpassword")
+NEO4J_AUTH = os.getenv("NEO4J_AUTH", "neo4j/testpassword").split('/')
 EMBEDDING_DIM = 300
 HIDDEN_DIM = 128
 __version__ = "0.1.0"
